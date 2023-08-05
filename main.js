92ca2468e55f1aecbb23c707cb6513219f18a670
Michelle <98421772+ChellT@users.noreply.github.com>
2023-08-05 22:19:04 +0100

    added clickable function
    added a clickable button for start in which when the user enters 1 it will say "you entered the number 1" and if the user enters 2 it will say "you entered the number 2"

---
+++ main.js
@@ -0,0 +1,8 @@
+function StarterButton() { 
+    var journey = prompt("testing!");
+    if(journey == 1){
+        alert("you entered the number 1")
+    } else {
+        alert("you entered the number 2")
+    }
+}
